ce8e5df388a6b6453fa06d2cd22ffc8e3bfa80fe
Felix <rojofelix20@gmail.com>
2024-05-15 12:48:32 +0200

    Metodo restar

---
+++ src/pktCalculo/Restar.java
@@ -0,0 +1,14 @@
+package pktCalculo;
+
+import java.util.Scanner;
+
+public class Restar {
+	
+	public static void resta(float num1, float num2) {
+        Scanner ent = new Scanner(System.in); // Entrada de datos
+
+        float resultado = num1 - num2;
+        System.out.println("El resultado de la resta es: " + resultado);
+    }
+
+}
